e73a8c67242accf0e976ce57e743b050909cfede
Anna <iaanya27@gmail.com>
2024-06-21 12:05:52 +0300

    added loader and markup

--- src/main.js
+++ src/main.js
@@ -3,32 +3,31 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import getImages from './js/pixabay-api.js';
+import { imagesTamplate } from './js/render-functions.js';
+
+const refs = {
+  formEl: document.querySelector('.form-request'),
+  btnEl: document.querySelector('.btn'),
+  ulElem: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
 
 const params = new URLSearchParams({
   key: '44439688-e504d8637fbba129a9d0d5115',
-  q: '',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
 });
-
-function getImages(url) {
-  fetch(url).then(res => {
-    if (!res.ok) throw new Error(res.status);
-    return res.json();
-  });
-}
-
-const refs = {
-  formEl: document.forms[0],
-  btnEl: document.querySelector('.btn'),
-  ulElem: document.querySelector('.gallery'),
+const options = {
+  captionDelay: 250,
 };
 
 refs.formEl.addEventListener('submit', e => {
   e.preventDefault();
-  const usertext = e.currentTarget.elements.query.value.trim();
-  if (!usertext) {
+  const userText = e.currentTarget.elements.query.value.trim();
+
+  if (!userText) {
     iziToast.warning({
       position: `topRight`,
       title: `Attention`,
@@ -36,15 +35,22 @@ refs.formEl.addEventListener('submit', e => {
     });
     return;
   }
-  params.set('q', usertext);
+  params.set('q', userText);
   const userUrl = 'https://pixabay.com/api/?' + params;
 
   getImages(userUrl)
     .then(images => {
-      console.log(images);
-      if (!images.hits.length) return Promise.reject('error');
+      refs.loader.classList.remove('js-hidden');
+      setTimeout(() => {
+        refs.loader.classList.add('js-hidden');
+        console.log(images);
+        if (!images.hits.length) return Promise.reject('error');
+        const markup = imagesTamplate(images.hits);
+        refs.ulElem.innerHTML = markup;
+        const lightbox = new SimpleLightbox('.gallery a', options);
+        lightbox.refresh();
+      }, 2000);
     })
-
     .catch(error => {
       iziToast.error({
         position: `topRight`,
@@ -54,24 +60,3 @@ refs.formEl.addEventListener('submit', e => {
       });
     });
 });
-
-// function imageTamplate(image) {
-//   return `<li class="gallery-item">
-//             <a class="gallery-link" href=${image.original}>
-//                 <img class="gallery-image"
-//                 src=${image.preview}
-//                 alt=${image.description}
-//                 title=${image.description} />
-//             </a>
-//         </li>`;
-// }
-// function imagesTamplate(arr) {
-//   return arr.map(imageTamplate).join('');
-// }
-// const markup = imagesTamplate(images);
-// ulElem.innerHTML = markup;
-
-// const options = {
-//   captionDelay: 250,
-// };
-// const lightbox = new SimpleLightbox('.gallery a', options);
